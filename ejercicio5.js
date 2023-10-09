2e412a81ac92a025c2174cd63f78d6d82c3444ec
AlejandroP75 <diego_palacio5@hotmail.com>
2023-10-09 15:13:42 -0500

    Segundo commit

--- ejercicio5.js
+++ ejercicio5.js
@@ -4,6 +4,14 @@ let autLib = document.getElementById("autLib");
 let anioLib = document.getElementById("anioLib");
 let botAniadir = document.getElementById("aniaLib");
 let form = document.getElementById("biblio");
+let libros = [];
+let libro1 = {
+    img: "libreria/la oscurdiad de los colores.jpg",
+    titulo: "La oscuridad de los colores",
+    autor: "Martin Blasco",
+    año: "2015",
+    estado: "Prestado",
+    prestadoA: "Juan Camilo"}
 
 function añadirLibro(){
     console.log("This is a test");
@@ -19,11 +27,35 @@ function añadirLibro(){
     portadaE.setAttribute("width", "200px");
     portadaE.setAttribute("height", "200px");
     let textE = document.createElement("p");
-    textE.innerHTML = "<br>Titulo: " + titulo + "<br>Autor: " + autor + "<br>Año: " + anio;
+    let disponible = '<button class="btnLibro">Devolver</button>';
+    if (prestado == "no"){
+        disponible = '<button class="btnLibro">Prestar</button>';
+    }
+
+    textE.innerHTML = '<h1 class="titLibro">' + titulo + '</h1><button class="btnLibro">Ver mas</button>' + disponible + '<br><button class="btnLibro">Eliminar</button>';
   
     form.appendChild(contenedor);
     contenedor.appendChild(portadaE);
-    contenedor.appendChild(textE);    
+    contenedor.appendChild(textE); 
 }
 
+    function cargar(){
+        let contenedor = document.createElement("div");
+        contenedor.setAttribute("class", "libro");
+        let portadaE = document.createElement("img");
+        portadaE.setAttribute("src", libro1["img"]);
+        portadaE.setAttribute("width", "200px");
+        portadaE.setAttribute("height", "200px");
+        let textE = document.createElement("p");
+        let disponible = '<button class="btnLibro">Devolver</button>';
+        if ( libro1["estado"]== "Prestado"){
+            disponible = '<button class="btnLibro">Prestar</button>';
+        }
+        textE.innerHTML = '<h1 class="titLibro">' + libro1["titulo"] + '</h1><button class="btnLibro">Ver mas</button>' + disponible + '<br><button class="btnLibro">Eliminar</button>';
+        form.appendChild(contenedor);
+        contenedor.appendChild(portadaE);
+        contenedor.appendChild(textE); 
+    }
 botAniadir.addEventListener("click", añadirLibro);
+
+window.addEventListener("load", cargar);
